24e8b5caf510ee89a8c278bc7088e8fae50d3274
Narayan Prusty <arsenal.narayan@gmail.com>
2023-05-08 13:02:23 +0100

    commented mint and deploy

--- deploy/1-deploy.ts
+++ deploy/1-deploy.ts
@@ -1,14 +1,13 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import hre from "hardhat";
 import { ethers } from "hardhat";
+import { CollectionDeployer } from "../typechain-types";
 
 module.exports = async ({getNamedAccounts, deployments} : HardhatRuntimeEnvironment) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  console.log(deployer)
-
-  await deploy("CollectionDeployer", {
+  const result = await deploy("CollectionDeployer", {
     contract: "CollectionDeployer",
     from: deployer,
     log: true,
@@ -21,6 +20,18 @@ module.exports = async ({getNamedAccounts, deployments} : HardhatRuntimeEnvironm
       },
     },
   });
+
+  // const collectionDeployer:CollectionDeployer = await ethers.getContractAt("CollectionDeployer", result.address)
+  // let tx = await collectionDeployer.deployCollection("Test", "TST", "http://test.com/")
+  // const receipt = await tx.wait(1)
+  // const collectionAddress = receipt.events[4].args.collection;
+
+  // console.log("Collection address is: ", collectionAddress)
+
+  // tx = await collectionDeployer.mint(collectionAddress, "0xFbaec94e7663eb57D0196206620a36FC131c943C", 1);
+  // await tx.wait(1)
+
+  // console.log("Minted token")
 }
 
 module.exports.tags = ['deploy'];
